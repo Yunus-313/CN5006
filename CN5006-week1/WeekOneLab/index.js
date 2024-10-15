6f0292f525513875a636d86b0c92d2c60419f68b
Yunus-313 <133156553+Yunus-313@users.noreply.github.com>
2024-10-15 17:56:44 +0100

    Add files via upload

---
+++ CN5006-week1/WeekOneLab/index.js
@@ -0,0 +1,15 @@
+console.log("This is my first programme!!")
+console.log("Welcome Yunus, you made $88,000 last year")
+const num1 = 10;
+const num2 = 2;
+const sum = (num1 + num2);//Adding two numbers
+//Programme to output sum of two numbers
+console.log("The sum of " + num1 + " " + num2 + " is: " + sum )
+
+console.log(num1 + " divided by " + num2 + " is " + (num1/num2))
+console.log(num1 + " multiplied by " + num2 + " is " + (num1*num2))
+console.log(num1 + " + " + num2 + " is " + (num1 + num2))
+console.log(num1 + " minus " + num2 + " is " + (num1 - num2))
+
+
+
